40df0644bd0442f43e7b99a76d24a4d8e83ab853
Rob-Swensen <rg.swensen801@gmail.com>
2020-03-11 15:13:37 -0600

    final commit

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express'),
+massive = require('massive'),
+{SERVER_PORT, CONNECTION_STRING} = process.env,
+ctrl = require ('./products_controller')
+
+
+app = express();
+
+app.use(express.json());
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}) 
+    .then(db => {
+        app.set('db', db)
+        console.log('DB Connected')
+    })
+    .catch(err => console.log(err));
+
+app.get('/api/products', ctrl.getAll);
+app.get('/api/product/:id', ctrl.getOne);
+app.put('/api/products/:id', ctrl.update);
+app.post('/api/products', ctrl.create);
+app.delete('/api/products/:id', ctrl.delete)
+
+
+
+
+const port = SERVER_PORT;
+app.listen(port, () => console.log(`Server is running on ${port}`))
